c0d9c8f884d8db6c1c4b9a6bc7c5051ea9a56185
ramsukhsahu <ramsukhsahu@gmail.com>
2026-09-19 11:42:11 +0530

    Delete show_database.py

--- show_database.py
+++
@@ -1,8 +0,0 @@
-# import mysql.connector
-# con=mysql.connector.connect(host='localhost', user='root', passwd='')
-from connection import get_connection
-con=get_connection()
-mycursor = con.cursor()
-mycursor.execute('show databases')
-for db in mycursor:
-    print(db)
